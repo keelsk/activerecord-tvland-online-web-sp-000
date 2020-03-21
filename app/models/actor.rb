41fe0dfe6a79b1fd24e6214de1f88268db9830f2
Kirsten Keels <keelsk@gmail.com>
2020-03-21 14:59:59 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,11 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    
+    roles = []
+    self.characters.each do |character|
+      roles << "#{character.name} - #{character.show}"
+    end
+    roles
   end
   
 end
