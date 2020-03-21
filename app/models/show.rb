2d1522a5ed584e493386f56b6eea52154e2db429
Kirsten Keels <keelsk@gmail.com>
2020-03-21 15:11:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   def actors_list
     actors = []
     self.characters.each do |character|
-      actors << "#{character.name} - #{character.show.name}"
+      actors << "#{character.actor} - #{character.show.name}"
     end
     actors
   end
