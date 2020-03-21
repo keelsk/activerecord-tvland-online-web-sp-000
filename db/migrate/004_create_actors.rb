e590a263ed8caa42bebd65400940b3bf1ab2e39b
Kirsten Keels <keelsk@gmail.com>
2020-03-21 14:49:48 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,8 @@
 class AddFirstLastNamesToActors < ActiveRecord::Migration[5.2]
   def change
-    add_column :actors, :first_name, :string
-    add_column :actors, :last_name, :string
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
